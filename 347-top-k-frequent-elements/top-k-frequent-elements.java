14ce5d75917b78ca5c8c40cbefec8069c599b89c
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-11-24 00:40:14 +0530

    Time: 63 ms (5.15%) | Memory: 48.6 MB (47.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,33 +1,23 @@
 class Solution {
-    class Number implements Comparable<Number>{
-        int element;
-        int frequency;
-        public Number(int element, int frequency){
-            this.element = element;
-            this.frequency=frequency;
-        }
-        // override the method that comparre
-        public int compareTo(Number that){
-            return that.frequency - this.frequency; // sorted in descending order or decreasing order
-        }
-
-    }
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        PriorityQueue<Number> pq = new PriorityQueue<>();
-        for(int num:nums){
-            freq.put(num,freq.getOrDefault(num,0)+1);
+        HashMap<Integer,Integer>map = new HashMap<>();
+       for(int num:nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+       }
+       int[] result = new int[k];
+       for(int i=0; i<k; i++){
+        int maxFrequency =-1;
+        int maxFrequentElement =-1;
+        for(int key:map.keySet()){
+            if(map.get(key)>maxFrequency){
+                maxFrequency = map.get(key);
+                maxFrequentElement = key;
+            }
         }
-        // now put get the element fromt the map with freq and put(offer) them into PriorityQueue
-        for(int key:freq.keySet()){
-            pq.offer(new Number(key,freq.get(key)));
-        }
-        // now get the most freqency element from the priorityQueue and add them inside the result array
-        int [] result = new int [k];
-        for(int i=0; i<k; i++){
-            result[i] = pq.poll().element;
-        }
-        return result;
+        result[i]= maxFrequentElement;
+        map.remove(maxFrequentElement);
+       }
+       return result;
         
     }
 }
