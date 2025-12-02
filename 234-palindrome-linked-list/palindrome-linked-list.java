0a0889a78cd0a3aec2deb3a2e9001926f0756f40
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-12-03 02:51:57 +0530

    Time: 15 ms (9.93%) | Memory: 101.9 MB (5.97%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,25 +9,23 @@
  * }
  */
 class Solution {
-    private ListNode start;
-
     public boolean isPalindrome(ListNode head) {
-        start = head;           // This will move from the beginning
-        return checkPalindrome(head); // Start checking from the end
-    }
-
-    private boolean checkPalindrome(ListNode end) {
-        if (end == null) return true;
-
-        // Recursively go to the end of the list
-        if (!checkPalindrome(end.next)) return false;
-
-        // Compare current start and end values
-        if (start.val != end.val) return false;
-
-        // Move start pointer forward
-        start = start.next;
-
+        Stack<Integer>stack= new Stack<>();
+        ListNode temp = head;
+        while(temp!=null){
+            stack.push(temp.val);
+            temp = temp.next;
+            
+        }
+        // reset the head
+        temp = head;
+        while(temp!=null){
+            if(temp.val!=stack.pop()){
+                return false;
+            }
+            temp =temp.next;
+        }
         return true;
+        
     }
 }
